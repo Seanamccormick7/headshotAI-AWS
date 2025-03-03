fc651caad6da22bf0fab898e67505dce62cb578a
Sean <seanamccormick7@gmail.com>
2025-03-03 00:03:23 -0800

    increased lr

--- predict.py
+++ predict.py
@@ -51,8 +51,8 @@ def run_training(
         "--mixed_precision=fp16",
         "--use_8bit_adam",              #to reduce memory usage
         "--gradient_checkpointing",
-        "--learning_rate=1e-6",     #or 2e-6 if not working well
-        "--text_encoder_lr=3e-6",
+        "--learning_rate=3e-6",     #or 2e-6 if not working well
+        "--text_encoder_lr=5e-6",
         "--lr_warmup_steps=100",
         "--lr_scheduler=constant",
         "--seed=40",
